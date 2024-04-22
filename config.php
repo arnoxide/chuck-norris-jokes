f599e0e911b8656a58237dc28ed79ee5f0395052
Masutha Arnold <85495755+arnoxide@users.noreply.github.com>
2024-04-23 04:08:33 +1400

    ...

--- config.php
+++ config.php
@@ -1,22 +1,9 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'chuck_norris_jokes');
-
-// Database connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$connect = new mysqli('db', 'php_docker', 'password', 'chuck_norris_jokes');
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
 }
 
-// $connect = new mysqli('db', 'php_docker', 'password', 'chuck_norris_jokes');
-
-// // Check connection
-// if ($connect->connect_error) {
-//     die("Connection failed: " . $connect->connect_error);
-// }
-
-// $conn = $connect;
+$conn = $connect;
