f62edd0b6acf5733320a33789c02d542a13b96e0
Masutha Arnold <85495755+arnoxide@users.noreply.github.com>
2024-05-13 11:37:17 +1400

    ...

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
 <?php
-$connect = new mysqli('db', 'php_docker', 'password', 'chuck_norris_jokes');
+// $connect = new mysqli('db', 'php_docker', 'password', 'chuck_norris_jokes');
+
+// // Check connection
+// if ($connect->connect_error) {
+//     die("Connection failed: " . $connect->connect_error);
+// }
+
+// $conn = $connect;
+
+$connect = new mysqli('localhost', 'root', '', 'chuck_norris_jokes');
 
 // Check connection
 if ($connect->connect_error) {
